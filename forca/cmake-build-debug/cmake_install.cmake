6d6b1057c1161e4aa82bc94dac12a3de814a7481
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-02-21 18:56:36 +0000

    updates

--- forca/cmake-build-debug/cmake_install.cmake
+++ forca/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Programacao/estudando-c/forca
+# Install script for directory: D:/Programacao/GitHub/estudando-c/forca
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Programacao/estudando-c/forca/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Programacao/GitHub/estudando-c/forca/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- forca/forca.c
+++ forca/forca.c
@@ -1,40 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-    char palavrasecreta[20];
+// variaveis globais
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
 
+// funcoes
+void abertura() {
+    printf("*************************\n");
+    printf("*     Jogo da Forca     *\n");
+    printf("*************************\n\n");
+}
+
+void chuta() {
+    char chute;
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+    tentativas++;
+}
+
+int jachutou(char letra) {
+    int achou = 0;
+
+    for (int j = 0; j < tentativas; j++) {
+        if (chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca() {
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+        // a letra já foi chutada?
+        int achou = jachutou(palavrasecreta[i]);
+
+        if (achou) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+    }
+    printf("\n");
+}
+
+void escolhepalavra() {
     sprintf(palavrasecreta, "MELANCIA");
+}
 
+// main
+int main() {
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
+    escolhepalavra();
+
+    abertura();
 
     do {
-        for(int i = 0; i < strlen(palavrasecreta); i++) {
-            int achou = 0;
+        // imprime a palavra secreta
+        desenhaforca();
 
-            for (int j = 0; j < tentativas; j++) {
-                if (chutes[j] == palavrasecreta[i]) {
-                    achou = 1;
-                    break;
-                }
-            }
-            if (achou) {
-                printf("%c ", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-        printf("\n");
-
-        char chute;
-        scanf(" %c", &chute);
-
-        chutes[tentativas] = chute;
-        tentativas++;
+        // captura um novo chute
+        chuta();
 
     } while (!acertou && !enforcou);
 }
